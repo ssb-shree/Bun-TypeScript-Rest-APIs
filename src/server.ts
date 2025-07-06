280c699b14a7ea01ceb77ebb67d558ffd051bc9b
ssb-shree <bavachikarshree1666@gmail.com>
2025-07-06 17:25:00 +0530

    update server to only start when a db connection is available

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log("Hello Bun");
+import config from "config";
+
+import app from "./app";
+import connectDB from "./utils/db";
+import logger from "./utils/logger";
+
+const port = config.get<number>("port");
+
+const startServer = async () => {
+  const db = await connectDB();
+
+  if (db) {
+    app.listen(port, () => logger.info(`Server is running at ${port}, DB connected to host ${db.connection.host}`));
+  } else {
+    logger.fatal("Shutting down the server");
+    process.exit(1);
+  }
+};
+
+startServer();
