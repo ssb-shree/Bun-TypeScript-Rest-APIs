a8815800a5efed072e10686068544922c1de2b6b
ssb-shree <bavachikarshree1666@gmail.com>
2025-07-08 18:46:53 +0530

    setup errorHandler middleware and other global middleware added a health-check route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,37 @@
-import express from "express";
+import express, { type NextFunction, type Request, type Response } from "express";
 
 import morgan from "morgan"; // http logger
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import config from "config";
+import { errorHandler } from "./middlewares/errorHandler";
+import asyncHandler from "./utils/asyncHandler";
+import { OK } from "./constants/status-codes";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(
-  morgan(
-    "\x1b[36m:date[web]\x1b[0m \x1b[33m:method\x1b[0m At(\x1b[34m:url\x1b[0m) Status[\x1b[32m:status\x1b[0m] - [\x1b[35m:response-time ms\x1b[0m]"
-  )
+  cors({
+    origin: config.get<string>("origin"),
+    credentials: true,
+  })
 );
+app.use(cookieParser());
+
+app.use(
+  morgan("\x1b[36m:date[web]\x1b[0m \x1b[33m:method\x1b[0m (\x1b[34m:url\x1b[0m) Status[\x1b[32m:status\x1b[0m] - [\x1b[35m:response-time ms\x1b[0m]")
+);
+
+// health check route
+app.get(
+  "/ping",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => res.status(OK).send("pong"))
+);
+
+app.use(errorHandler);
 
 export default app;
