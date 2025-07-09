4539b0226dbdaf2abc1c3f36066bf8a24a927c5d
ssb-shree <bavachikarshree1666@gmail.com>
2025-07-09 18:59:04 +0530

    add new parammeters

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,8 @@
 export default {
+  status: process.env.STATUS,
   port: process.env.PORT || 8080,
   uri: process.env.MONGODB_URI,
+  origin: process.env.STATUS === "PROD" ? process.env.CLIENT_URL : process.env.DEV_CLIENT_URL,
+  accessSecret: process.env.JWT_ACCESS_SECRET,
+  refreshSecret: process.env.JWT_REFRESH_SECRET,
 };
