a6c3608061a0125ec49ead2e01388963a197eab9
ssb-shree <bavachikarshree1666@gmail.com>
2025-07-10 20:14:27 +0530

    minor changes

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,7 +3,7 @@ import config from "config";
 
 import type { NextFunction, Request, Response } from "express";
 
-export type DecodedToken = {
+export type accessTokenPayload = {
 	userID: string;
 	sessionID: string;
 	iat?: number;
@@ -12,7 +12,7 @@ export type DecodedToken = {
 
 // attaching a new property to the express req object
 export interface AuthenticatedRequest extends Request {
-	user?: DecodedToken;
+	user?: accessTokenPayload;
 }
 
 import asyncHandler from "../utils/asyncHandler";
@@ -21,13 +21,13 @@ import { UNAUTHORIZED } from "../constants/status-codes";
 import logger from "../utils/logger";
 
 export const checkAuth = asyncHandler(async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-	const { accessToken, refreshToken } = req.cookies;
+	const accessToken = req.cookies.accessToken as string | undefined;
 	if (!accessToken) {
 		logger.warn("access token not found");
 		throw new ApiError(UNAUTHORIZED, "No Token provided");
 	}
 
-	const decodedPayload = jwt.verify(accessToken, config.get<string>("accessSecret")) as DecodedToken;
+	const decodedPayload = jwt.verify(accessToken, config.get<string>("accessSecret")) as accessTokenPayload;
 	if (!decodedPayload) {
 		logger.warn("failed to decode the jwt token");
 		throw new ApiError(UNAUTHORIZED, "Unauthorized");
