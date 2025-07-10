231827bd5becac2eb658a4be8fe22f23c6c4d7d7
ssb-shree <bavachikarshree1666@gmail.com>
2025-07-10 13:34:15 +0530

    fix conversion to milisecond

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,7 @@
 export const daysFromNow = (numberOfDays: number): Date => {
-  return new Date(Date.now() + numberOfDays * 24 * 60 * 60);
+	return new Date(Date.now() + numberOfDays * 24 * 60 * 60 * 1000);
 };
 
 export const minsFromNow = (numberOfMins: number): Date => {
-  return new Date(Date.now() + numberOfMins * 60);
+	return new Date(Date.now() + numberOfMins * 60 * 1000);
 };
