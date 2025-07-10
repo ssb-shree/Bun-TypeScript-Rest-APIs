f488cd5511465aa9f97d5ed3eafdef2a7efc4a81
ssb-shree <bavachikarshree1666@gmail.com>
2025-07-10 12:29:39 +0530

    add zod schema to validate req.body

---
+++ src/controllers/auth.schema.ts
@@ -0,0 +1,19 @@
+import z from "zod";
+
+const loginSchema = z.object({
+	email: z.string().email().min(1).max(255),
+	username: z.string().min(1).max(255),
+	password: z.string().min(6).max(255),
+	userAgent: z.string().optional(),
+});
+
+const registerSchema = loginSchema
+	.extend({
+		confirmPassword: z.string().min(6).max(255),
+	})
+	.refine(data => data.password === data.confirmPassword, {
+		message: "passwords did not match",
+		path: ["confirmPassword"],
+	});
+
+export { registerSchema, loginSchema };
