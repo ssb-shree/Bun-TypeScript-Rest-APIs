586a749249efa547ddf578be0d11cfd91961a5db
ssb-shree <bavachikarshree1666@gmail.com>
2025-07-13 08:37:36 +0530

    update

--- config/default.ts
+++ config/default.ts
@@ -1,8 +1,10 @@
 export default {
-  status: process.env.STATUS,
-  port: process.env.PORT || 8080,
-  uri: process.env.MONGODB_URI,
-  origin: process.env.STATUS === "PROD" ? process.env.CLIENT_URL : process.env.DEV_CLIENT_URL,
-  accessSecret: process.env.JWT_ACCESS_SECRET,
-  refreshSecret: process.env.JWT_REFRESH_SECRET,
+	status: process.env.STATUS,
+	port: process.env.PORT || 8080,
+	uri: process.env.MONGODB_URI,
+	origin: process.env.STATUS === "PROD" ? process.env.CLIENT_URL : process.env.DEV_CLIENT_URL,
+	accessSecret: process.env.JWT_ACCESS_SECRET,
+	refreshSecret: process.env.JWT_REFRESH_SECRET,
+	resendApiKey: process.env.RESEND_API_KEY,
+	fromAddress: process.env.FROM,
 };
